206917a03d4ccaa775a56d58c99120d10d31b515
patrickniyogitare28 <patrickniyogitare28@gmail.com>
2021-11-16 10:51:11 +0200

    chore: added app nav

---
+++ src/components/counter/app-nav/index.tsx
@@ -0,0 +1,14 @@
+import React, {FC} from "react";
+import GithubIcon from '@assets/icons/github.png';
+import styles from './styles.module.scss';
+
+const AppNav: FC = () => {
+    return (
+        <div className={styles.appNavContainer}>
+        <a href="https://github.com/PatrickNiyogitare28/super-vite.git" target="_blank">
+           <img src={GithubIcon} alt="Github icon" />
+        </a>
+        </div>
+    )
+}
+export default AppNav;
